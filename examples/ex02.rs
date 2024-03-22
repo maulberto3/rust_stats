604384b295c22eca559ad55e696ef7249043ba78
Mauricio <maulberto3@hotmail.com>
2024-03-21 20:54:04 -0600

    updating for median

--- examples/ex02.rs
+++ examples/ex02.rs
@@ -1,6 +1,7 @@
 use anyhow::Result;
 use csv::{Reader, ReaderBuilder};
 use reqwest::Response;
+use serde::de;
 
 async fn fetch_data() -> Result<Vec<f32>> {
     let url =
@@ -21,13 +22,29 @@ async fn fetch_data() -> Result<Vec<f32>> {
 fn calc_mean(data: &Vec<f32>) -> Result<f32>{
     let sum: f32 = data.iter().sum();
     let res: f32 = sum / data.len() as f32;
-    dbg!(&res);
+    // dbg!(&res);
     Ok(res)
+}
+
+fn calc_median(data: &mut Vec<f32>) -> Result<f32>{
+    data.sort_unstable_by(|a, b| a.partial_cmp(b).unwrap());
+    let len = data.len();
+    if len % 2 == 0 {
+        let mid1 = data[(len/2)-1];
+        let mid2 = data[len/2];
+        Ok((mid1 + mid2) / 2.0)
+    }
+    else {
+        Ok(data[len/2])
+    }
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let data = fetch_data().await?;
-    let _x = calc_mean(&data);
+    let mut salaries = fetch_data().await?;
+    let mean_salaries = calc_mean(&salaries).unwrap();
+    dbg!(&mean_salaries);
+    let median_salaries = calc_median(&mut salaries).unwrap();
+    dbg!(&median_salaries);
     Ok(())
 }
